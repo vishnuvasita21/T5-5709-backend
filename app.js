f0380d11099645b27f375ca3f3d001b16c12d6df
vishnuvasita21 <vs369825@dal.ca>
2024-03-11 15:29:58 -0300

    new

--- app.js
+++ app.js
@@ -1,132 +1,119 @@
 const express = require('express');
-const app = express();
+const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
-const PORT = 8080;
+const app = express();
+const PORT = 8081;
 
 app.use(express.json());
 
-let userDetail = {
-    message: 'Users retrieved',
-    success: true,
-    users: [{
-        email: 'abc@abc.ca',
-        firstName: 'ABC',
-        id: '5abf6783'
-    },
-    {
-        email: 'xyz@xyz.ca',
-        firstName: 'XYZ',
-        id: '5abf674563'
-    }]
+const file = './user.json';
+
+function readUserData() {
+    const jsonData = fs.readFileSync(file);
+    return JSON.parse(jsonData);
 }
 
+function writeUserData(userData) {
+    const stringData = JSON.stringify(userData, null, 2);
+    fs.writeFileSync(file, stringData);
+}
 
-//get user all detail
+// Get all user details
 app.get('/users', (req, res) => {
-
     try {
-        res.status(200).json(userDetail);
-
+        const userDetail = readUserData();
+        res.status(200).json({
+            message: "Users retrieved",
+            success: true, userDetail
+        });
     } catch (err) {
-        res.status(400).json({ err })
+        res.status(500).json({ error: err.message });
     }
+});
 
-})
-
-
-//get user by id
-
+// Get user by id
 app.get("/users/:id", (req, res) => {
-
     try {
-        let userId = req.params.id;
+        const userId = req.params.id;
+        const userDetail = readUserData();
+        const userById = userDetail.users.find((user) => user.id === userId);
 
-        const userByid = userDetail.users.find((user) => user.id === userId);
-
-        if (!userByid) {
-            res.status(401).json({ message: 'User not found' });
+        if (!userById) {
+            res.status(404).json({ message: 'User not found' });
         } else {
-            res.status(200).json({ success: true, user: userByid });
+            res.status(200).json({ success: true, user: userById });
         }
-
     } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
+        res.status(500).json({ error: error.message });
     }
+});
 
-
-})
-
-
-//post
+// Add a new user
 app.post('/add', (req, res) => {
-
-
     try {
-        const newEmail = req.body.email
-        const newfirstName = req.body.firstName
+        const newEmail = req.body.email;
+        const newFirstName = req.body.firstName;
+        const userDetail = readUserData();
 
-        if (userDetail.users.find((user) => user.email === newEmail)) {
-            return res.status(409).json({ message: "This email is already in use." })
-        } else {
-
-            userDetail.users.push({
-                email: newEmail,
-                firstName: newfirstName,
-                id: uuidv4().slice(0, 8)
-            })
+        if (userDetail.users.some((user) => user.email === newEmail)) {
+            return res.status(409).json({ message: "This email is already in use." });
         }
-        res.status(200).send({
+
+        const newUser = {
+            email: newEmail,
+            firstName: newFirstName,
+            id: uuidv4().slice(0, 8)
+        };
+        userDetail.users.push(newUser);
+        writeUserData(userDetail);
+
+        res.status(201).json({
             message: 'User added',
             success: true,
-        })
+            user: newUser
+        });
     } catch (error) {
-        res.status(500).json({ error: error })
+        res.status(500).json({ error: error.message });
     }
+});
 
-})
-
-
-//put
+// Update user details
 app.put('/update/:id', (req, res) => {
-
     try {
-
-        const userId = req.params.id
-        const newEmail = req.body.email
-        const newFirstName = req.body.firstName
-
+        const userId = req.params.id;
+        const { email, firstName } = req.body;
+        const userDetail = readUserData();
         const userIndex = userDetail.users.findIndex(user => user.id === userId);
 
         if (userIndex !== -1) {
             userDetail.users[userIndex] = {
                 ...userDetail.users[userIndex],
-                email: newEmail,
-                firstName: newFirstName
+                email: email,
+                firstName: firstName
             };
-         
+            writeUserData(userDetail);
         } else {
             return res.status(404).json({ message: "No user with this id found" });
         }
 
         res.status(200).json({
             message: 'User updated',
-            success: true
-        })
+            success: true,
+            user: userDetail.users[userIndex]
+        });
     } catch (error) {
         res.status(500).json({
             error: error.message
         });
     }
-})
-
-
-//for anyother route
-app.all('*', (req, res) => {
-    res.status(404).json({  
-        message: 'Not found' 
-    })
 });
 
+// Handle any other routes
+app.all('*', (req, res) => {
+    res.status(404).json({ message: 'route not found' });
+});
+
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
